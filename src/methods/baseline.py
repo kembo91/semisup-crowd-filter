39f6a6f1d2425397319459c5353bfe54cec41fb5
JoJo <ketema.galy@gmail.com>
2018-10-26 17:46:03 +0300

    steps bugfix

--- src/methods/baseline.py
+++ src/methods/baseline.py
@@ -2,8 +2,8 @@ from src.utils.utils import OutlierDetector
 import numpy as np
 
 def process_baseline(net, testdata, anchordata):
-    train_preds = net.predict_generator(anchordata)
+    train_preds = net.predict_generator(anchordata, steps = len(anchordata))
     outdet = OutlierDetector(train_preds, 8)
-    test_preds = net.predict_generator(testdata)
+    test_preds = net.predict_generator(testdata, steps = len(testdata))
     connected = outdet.determine_connection(test_preds)
     return [int(x) for x in connected]
